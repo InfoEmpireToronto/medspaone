f2630004de74cc8218658b4682359deab50fcb93
Thomas <tganyuk86@gmail.com>
2019-05-01 16:33:36 -0400

    asd

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -30,16 +30,16 @@
 
             <div class="item grid-sizer col-6 col-sm-6 col-md-4 col-lg-3">
               <div class="box bg-white shadow p-20">
-                <figure class="main mb-20 overlay overlay1 rounded"><a href="/profile/{{$article->author_id}}#Articles">
+                <figure class="main mb-20 overlay overlay1 rounded"><a href="/profile/{{$article->user()->slug}}#Articles">
                   <img src="/storage/{{$article->image}}" alt="" /></a>
                   <figcaption>
                     <h5 class="text-uppercase from-top mb-0">Read more</h5>
                       <p></p>
                   </figcaption>
-                    <div class="category"><a href="/profile/{{$article->author_id}}#Articles">{{$article->getCategories()[0]->name}}</a></div>
+                    <div class="category"><a href="/profile/{{$article->user()->slug}}#Articles">{{$article->getCategories()[0]->name}}</a></div>
                 </figure>
                   
-                <h6 class="mb-0"><a href="/profile/{{$article->author_id}}#Articles">{{ucwords(strtolower($article->title))}}</a></h6>
+                <h6 class="mb-0"><a href="/profile/{{$article->user()->slug}}#Articles">{{ucwords(strtolower($article->title))}}</a></h6>
 
               </div>
               <!-- /.box -->
@@ -56,7 +56,7 @@
          <div class="space70"></div>
         <div class="pagination bg text-center">
           <div class="col-lg-12 text-center"><div class="space20"></div>
-            <a href="/profile/{{$user->id}}" class="btn">Back To Profile <i class="fa fa-caret-right"></i></a>
+            <a href="/profile/{{$article->user()->slug}}" class="btn">Back To Profile <i class="fa fa-caret-right"></i></a>
             </div>
           <!-- <ul>
             <li><a href="#" class="btn btn-white shadow"><i class="mi-arrow-left"></i></a></li>
