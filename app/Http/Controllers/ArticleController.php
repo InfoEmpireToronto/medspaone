949ae43e56dbcdcc000d485c796b62301a927ccd
Thomas <tganyuk86@gmail.com>
2019-05-01 16:51:45 -0400

    asd

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -73,7 +73,7 @@ class ArticleController extends Voyager\VoyagerController
         {
             $user = User::where('slug', $name)->first();
             if($user)
-                return view('articles',[ 
+                return view('videos',[ 
                     'articles' => Post::where('author_id', $user->id)->where('type','video')->get(),
                     'user' => $user
 
