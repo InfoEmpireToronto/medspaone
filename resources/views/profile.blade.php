2ebedbb63824b6d6dee12d29bde0174cf0f801f4
Thomas <tganyuk86@gmail.com>
2019-05-02 11:39:38 -0400

    asd

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -426,7 +426,7 @@
                     <p class="more text-uppercase color-dark">
             				  <div class="rounded">
                         <div class="map" data-lat='{{$location->lat}}' data-lon='{{$location->lon}}' data-title='{{$location->title}}' ></div>
-                        <br><i class="fa fa-map-marker"></i>{{$location->address}}</a>
+                        <br><i class="fa fa-map-marker"></i> {{$location->address}}</a>
             					  <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5761.109462578503!2d-79.471203!3d43.782101!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5b844e921b65acf5!2sInfoEmpire!5e0!3m2!1sen!2sca!4v1555101204228!5m2!1sen!2sca" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen ></iframe> -->
             				  </div>
 @if($location->phone)   
@@ -435,7 +435,7 @@
 @if($location->fax)
 <i class="fa fa-fax"></i> <a href="fax:{{$location->fax}}" class="color-dark">{{$location->fax}}</a><br>
 @endif
-<!-- <i class="fa fa-envelope-o"></i> <a href="mailto:{{$user->email}}" target="_blank" class="color-dark">{{$user->email}}</a></p> -->
+<i class="fa fa-envelope-o"></i> <a href="#" target="_blank" class="color-dark">Contact Us</a></p>
                   </div>
                   <!-- /.card-block -->
                 </div>
