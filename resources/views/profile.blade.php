647137d59c3f2e6b6b752c7a24ab49302e18ab6e
Thomas <tganyuk86@gmail.com>
2019-05-02 15:51:50 -0400

    asd

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -237,9 +237,13 @@
             </figure>
 			  <div class="px-3">
 			  <h5 class="m-0 mt-3 text-uppercase">{{ucwords(strtolower($beforeAfter->title))}}</h5>
-			  <p class="m-0">Short description</p>
-			  <div class="meta mb-3 text-capitalize"><span class="date">April 12, 2019</span> <span class="author">By MedSpa.One</span> <span><i class="fa fa-thumbs-up"></i> 10</span> <span class="category">Body</span>
-					</div>
+			  <p class="m-0">{{$beforeAfter->body}}</p>
+          <div class="meta mb-3 text-capitalize">
+            <span class="date">{{$beforeAfter->created_at->diffForHumans()}}</span> 
+            <span class="author">{{$beforeAfter->user()->company}}</span> 
+            <span><i class="fa fa-thumbs-up"></i> 10</span> 
+            <span class="category">{{$beforeAfter->getCategories()[0]->name}}</span>
+          </div>
 			  </div>
           </div>
 @endforeach
