f2fc50d46d307e3483f4da2b29fe078ea1faca8f
Thomas <tganyuk86@gmail.com>
2019-05-08 10:43:18 -0400

    asd

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -17,7 +17,7 @@ $keywords = "";
         <meta name="description" content="<?=$description?>" />
         <meta name="author" content="416-769-5250, www.infoempire.com" />
         <meta name="copyright" content="&copy;<?=date('Y');?>, MedSpa.us" />
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        <!-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> -->
 
         <title><?=$title?></title>
         <link rel="stylesheet" type="text/css" href="/style/css/bootstrap.min.css">
