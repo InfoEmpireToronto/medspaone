a481d7d8897c26a5c8587abd16052738daa73458
Thomas <tganyuk86@gmail.com>
2019-05-15 09:38:27 -0400

    asd

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -49,7 +49,7 @@ width: 50px;
 
 @section('js')
         <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> -->
-
+            <script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
 		<script src="https://medspa.one:8080/socket.io/socket.io.js"></script>
         <script>
 
