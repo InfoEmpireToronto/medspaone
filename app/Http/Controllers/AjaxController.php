3725b9babf51d2a8298a464f8c2b32293c89f67d
Thomas <tganyuk86@gmail.com>
2019-05-15 10:29:47 -0400

    asd

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -56,7 +56,7 @@ class AjaxController extends Voyager\VoyagerController
         // dd($out);
 
 
-        Sms::sendSMS('14163226000', $request['name'].' has a question, to start the conversation go to https://medspa.one/phys/chat');
+        Sms::sendSMS('14169907119', $request['name'].' has a question, to start the conversation go to https://medspa.one/phys/chat');
 
         return response()->json(['status'=>'success']);
 
