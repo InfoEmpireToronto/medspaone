7f41f9bf0995ce8967ff743f989999932bff605f
Thomas <tganyuk86@gmail.com>
2019-05-23 16:55:47 -0400

    asd

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,21 +3,25 @@
 
 @section('content')
 	<div class="wrapper gray-wrapper">
-      <div class="container inner">
+      <div class="container inner pt-sm-20">
         <h2 class="heading text-center">Search Results</h2>
       <div class="space20"></div>
         <div class="row">
-          <div class="col-lg-8 offset-lg-2">
+          <div class="col-lg-10 offset-lg-1">
+			   <ul class="image-list">
             @foreach($results as $result)
-
-              <div>
-                <h3><a href="/{{$result->type}}/{{$result->slug}}" >{{$result->title}}</a></h3>
-                <p>{!! substr(strip_tags($result->body), 0, 200) !!}...&nbsp<a href="/{{$result->type}}/{{$result->slug}}">Read More</a></p>
+			  
 
 
-
-              </div>
+              <li>
+                  <figure class="rounded"><a href="/{{$result->type}}/{{$result->slug}}" ><img src="/storage/{{$result->image}}" alt="{{$result->title}}"></a></figure>
+                  <div class="post-content">
+                    <h6 class="post-title"> <a href="/{{$result->type}}/{{$result->slug}}" >{{$result->title}}</a></h6>
+                <p>{!! substr(strip_tags($result->body), 0, 200) !!}...&nbsp;<a href="/{{$result->type}}/{{$result->slug}}">Read&nbsp;more <i class="fa fa-angle-right"></i></a></p>
+				  </div>
+                </li>
             @endforeach
+			  </ul>
          </div>
           <!-- /column -->
         </div>
