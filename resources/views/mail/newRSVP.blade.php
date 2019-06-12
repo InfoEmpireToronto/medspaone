ae87051ac9a675dc8fd9b88bec98d883b1c80cb8
Thomas <tganyuk86@gmail.com>
2019-06-12 12:51:42 -0400

    asd

--- resources/views/mail/newRSVP.blade.php
+++ resources/views/mail/newRSVP.blade.php
@@ -1,4 +1,6 @@
-New RSVP Submitted at MedSpa.one
---------------------------
-<pre>{{print_r($data,1)}}</pre>
---------------------------
+New RSVP Submitted at MedSpa.one\n
+Name: {{$data['fname']}} {{$data['lname']}}\n
+Email: {{$data['email']}}\n
+Phone: {{$data['phone']}}\n
+Source: {{$data['source']}}\n
+Guests: {{$data['guests']}}\n
