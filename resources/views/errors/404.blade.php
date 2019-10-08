87356e627f2480d405c51bcbc02759e0c1ec627b
Tomas <tganyuk86@gmail.com>
2019-10-08 15:18:25 -0400

    asd

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,43 @@
-@extends('errors::minimal')
+@extends('layouts.main')
 
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+
+@section('content')
+	<div class="wrapper light-wrapper">
+      <div class="container inner pt-60 pt-sm-20">
+        <div class="boxed">
+          <div class="bg-white shadow rounded">
+            <div class="image-block-wrapper">
+              <div class="image-block col-lg-6">
+                <div class="image-block-bg bg-image" data-image-src="style/images/art/me1.jpg"></div>
+              </div>
+              <!--/.image-block -->
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-lg-6 offset-lg-6">
+                    <div class="box d-flex">
+                      <div class="align-self-center">
+						<h1 class="m-0">404</h1>
+						  <h5>The page you are looking for doesn't exist, click <a href="/">HERE</a> to go back.</h5>
+						  <div class="space30"></div>
+						  
+                      
+                      </div>
+                    </div>
+                    <!-- /.box -->
+                  </div>
+                  <!--/column -->
+                </div>
+                <!--/.row -->
+              </div>
+              <!--/.container-fluid -->
+            </div>
+            <!--/.image-block-wrapper -->
+          </div>
+          <!-- /.bg -->
+        </div>
+        <!-- /.boxed -->
+      </div>
+      <!-- /.container -->
+    </div>
+  
+@endsection
